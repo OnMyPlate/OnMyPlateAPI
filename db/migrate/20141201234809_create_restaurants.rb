a50f302b6f7e100c23a109ed33d59a4cb7c01f3e
cyurtbil <cenky92@gmail.com>
2014-12-01 18:49:01 -0500

    created restaurants migration

---
+++ db/migrate/20141201234809_create_restaurants.rb
@@ -0,0 +1,10 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.belongs_to :post, index: true
+    end
+  end
+end
