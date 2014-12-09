66f0ea59e0e1bbe03e780affa8123fc2c391683c
cyurtbil <cenky92@gmail.com>
2014-12-09 13:07:39 -0500

    added method that gets post params, and added post create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:name, :rating, :image, :review, :bookmarked, :user_id)
+    end
 end
