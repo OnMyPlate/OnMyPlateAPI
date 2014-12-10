e6b869b0f27650564a8981a4acf284457302bbac
cyurtbil <cenky92@gmail.com>
2014-12-10 11:19:57 -0500

    added signkey method to amazon controller and configured on the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   post '/login', to: 'users#login'
   get '/logout', to: 'users#logout'
+
+  get 'amazon/sign_key'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
