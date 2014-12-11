fbc24e0c5be1092d5fe45301939a78e53fe098bd
cyurtbil <cenky92@gmail.com>
2014-12-11 11:32:41 -0500

    added index and food image params method to the controller

--- app/controllers/food_images_controller.rb
+++ app/controllers/food_images_controller.rb
@@ -1,2 +1,10 @@
 class FoodImagesController < ApplicationController
+  def index
+    @food_images = FoodImage.all
+  end
+
+  private
+    def food_image_params
+      params.require(:food_image).permit(:image_url, :post_id, :food_id)
+    end
 end
