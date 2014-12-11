a66ae33f648166f5116592ebdfc8f11afd284de8
cyurtbil <cenky92@gmail.com>
2014-12-11 15:10:28 -0500

    now posts have user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = get_user(get_token).id
 
     if @post.save
       render json: @post, status: :created
@@ -16,6 +17,14 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:name, :rating, :image, :review, :user_id)
+      params.require(:post).permit(:rating, :review, :user_id, :food_id)
+    end
+
+    def get_token
+      token = request.headers.env['HTTP_AUTHORIZATION'].gsub(/Token token=/, "")
+    end
+
+    def get_user(token)
+      User.where(token: token)[0]
     end
 end
