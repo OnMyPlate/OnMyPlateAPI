5d2adda265728fedc79e1b1b7032bfa635378483
cyurtbil <cenky92@gmail.com>
2015-01-09 14:25:17 -0500

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,7 @@ class UserMailer < ActionMailer::Base
   def user_email(user_params)
     @username = user_params[:username]
     @email = user_params[:email]
-    @url = "http://localhost:3000/confirm_email/#{@username}"
-    # onmyplate.herokuapp.com
+    @url = "https://onmyplate.herokuapp.com/confirm_email/#{@username}"
     mail(to: @email, 
          date: Time.now,
          content_type: 'text/html',
