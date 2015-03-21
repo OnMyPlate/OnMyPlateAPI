3a6282071ba492196b04fc8d237cb67ae8cf9a26
cyurtbil <cenky92@gmail.com>
2015-03-21 15:06:03 -0400

    deleted code in likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,8 +11,6 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(like_params)
 
-    # @like.user_id = get_user(get_token).id
-
     if @like.save
       render json: @like, status: :created
     else
@@ -36,9 +34,4 @@ class LikesController < ApplicationController
     def get_token
       token = request.headers.env['HTTP_AUTHORIZATION'].gsub(/Token token=/, "")
     end
-
-    # def get_user(token)
-    #   User.where(token: token)[0]
-    # end
-
 end
